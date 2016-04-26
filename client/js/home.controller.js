77468837ab72db7fc346b881e5a8514c86374f55
Alex Quang <alexquang1991@gmail.com>
2016-04-25 21:49:47 -0700

    got typeText working

--- client/js/home.controller.js
+++ client/js/home.controller.js
@@ -3,18 +3,23 @@
 		.module('myApp')
 		.controller('homeCtrl', homeCtrl);
 
-	function homeCtrl() {
+	function homeCtrl($interval) {
 		var vm = this;
 
 		// Bound variables
 		vm.header = [{ message: 'Crime Data' }, { message: 'With Google Maps' }];
+		vm.words = ["Hello!", "Fill out the form below to get some crime information!"];
+		vm.wordCount = 0;
+		vm.charCount = 0;
+		vm.instructions = "";
 		vm.cIndex = 0;
 		vm.newSearch = {};
 		vm.switchHeaders = switchHeaders;
 		vm.startSearch = startSearch;
+		vm.typeText = typeText;
 
 		// Function implementations
-		function switchHeaders(current) {
+		function switchHeaders() {
 			if (vm.cIndex === 0)
 			{
 				vm.cIndex = 1;
@@ -28,5 +33,24 @@
 		function startSearch() {
 			console.log(vm.newSearch);
 		}
+
+		function typeText() {
+			vm.instructions = vm.instructions + vm.words[vm.wordCount][vm.charCount++];
+
+			if (vm.charCount > vm.words[vm.wordCount].length)
+			{
+				vm.wordCount++;
+				vm.charCount = 0;
+				vm.instructions = "";
+
+				if (vm.wordCount == vm.words.length)
+				{
+					vm.wordCount = 0;
+				}
+			}
+		}
+
+		// Function calls
+		$interval(vm.typeText, 130);
 	}
 } )(angular);
