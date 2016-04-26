0f7579b0db98d90a5f461bb2876470cba46ae73d
Alex Quang <alexquang1991@gmail.com>
2016-04-26 15:48:23 -0700

    started working with autocomplete

--- client/js/home.controller.js
+++ client/js/home.controller.js
@@ -13,10 +13,21 @@
 		vm.charCount = 0;
 		vm.instructions = "";
 		vm.cIndex = 0;
+		vm.placeSearch;
+		vm.autocomplete;
 		vm.newSearch = {};
+		vm.componentForm = {
+			street_number: 'short_name',
+			route: 'long_name',
+			locality: 'long_name',
+			administrative_area_level_1: 'short_name',
+			country: 'long_name',
+			postal_code: 'short_name'
+		};
 		vm.switchHeaders = switchHeaders;
 		vm.startSearch = startSearch;
 		vm.typeText = typeText;
+		vm.initAutocomplete = initAutocomplete;
 
 		// Function implementations
 		function switchHeaders() {
@@ -48,6 +59,10 @@
 					vm.wordCount = 0;
 				}
 			}
+		}
+
+		function initAutocomplete() {
+			vm.autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')), {types: ['geocode']});
 		}
 
 		// Function calls
