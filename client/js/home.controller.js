72513080ac24c1cfeac6d840cc6842b3a4a7f165
Alex Quang <alexquang1991@gmail.com>
2016-04-25 18:28:50 -0700

    created form on home page and added appropriate method in homeCtrl

--- client/js/home.controller.js
+++ client/js/home.controller.js
@@ -7,6 +7,26 @@
 		var vm = this;
 
 		// Bound variables
-		
+		vm.header = [{ message: 'Crime Data' }, { message: 'With Google Maps' }];
+		vm.cIndex = 0;
+		vm.newSearch = {};
+		vm.switchHeaders = switchHeaders;
+		vm.startSearch = startSearch;
+
+		// Function implementations
+		function switchHeaders(current) {
+			if (vm.cIndex === 0)
+			{
+				vm.cIndex = 1;
+			}
+			else
+			{
+				vm.cIndex = 0;
+			}
+		}
+
+		function startSearch() {
+			console.log(vm.newSearch);
+		}
 	}
 } )(angular);
