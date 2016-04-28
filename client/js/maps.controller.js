8a1288bf614c870e9f69e5b05922ad339789067e
Alex Quang <alexquang1991@gmail.com>
2016-04-27 17:24:44 -0700

    slightly refactored code and added circle around search radius

--- client/js/maps.controller.js
+++ client/js/maps.controller.js
@@ -18,13 +18,13 @@
 		function findCrimes() {
 			var crimesPromise = Search.find(vm.newSearch);
 			crimesPromise.then(function(result) {
-				console.log(result);
 				vm.crimeData = result;
 				vm.initMap(vm.newSearch);
 			});
 		}
 
 		function initMap(newSearch) {
+			var convRadius = newSearch.radius * 1600;
 
 			var mapOptions = {
 				center: new google.maps.LatLng(newSearch.lat, newSearch.lng),
@@ -35,9 +35,17 @@
 
 			vm.map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
+			var circle = new google.maps.Circle({
+				map: vm.map,
+				radius: convRadius,
+				fillColor: '#0099ff',
+				center: new google.maps.LatLng(newSearch.lat, newSearch.lng)
+			});
+
 			for (var crime in vm.crimeData) {
 				var crimeLoc = new google.maps.LatLng(vm.crimeData[crime].lat, vm.crimeData[crime].long);
 				var crimeDesc = vm.crimeData[crime].description;
+
 				addMarker(crimeLoc, crimeDesc);
 			}
 		}
@@ -59,12 +67,6 @@
 			});
 
 			vm.markers.push(marker);
-		}
-
-		function setMapOnAll(map) {
-			for (var i = 0; i < vm.markers.length; i++) {
-				vm.markers[i].setMap(map);
-			}
 		}
 
 		// Function calls
